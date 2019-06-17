d1ea990811af2c474eb6601642b5dd9bf092769e
satryopramahardi <pramahardi10@gmail.com>
2019-06-17 14:30:01 +0700

    first commit

---
+++ 1.py
@@ -0,0 +1,21 @@
+import sqlalchemy as db
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+engine = db.create_engine('mysql://root:admin@localhost/world')
+connection = engine.connect()
+
+asean_df = pd.read_sql_query("select * from country inner join asean on country.name=asean.negara", connection)
+
+sns.set_style("darkgrid")
+bar = sns.barplot(x='Name', y='Population', data=asean_df,  palette="muted")
+bar.set(xlabel='Negara', ylabel='Populasi (x100jt jiwa)')
+bar.set_xticklabels(bar.get_xticklabels(),rotation=30)
+bar.tick_params(labelsize=8)
+plt.title('Populasi Negara Asean')
+
+for p in bar.patches:
+    bar.text(p.get_x() + p.get_width()/2., p.get_height(), '%d' % int(p.get_height()),fontsize=8, color='gray', ha='center', va='bottom')
+
+plt.show()
---
+++ 4.py
@@ -0,0 +1,18 @@
+import sqlalchemy as db
+import pandas as pd
+import matplotlib.pyplot as plt
+
+engine = db.create_engine('mysql://root:admin@localhost/world')
+connection = engine.connect()
+
+asean_df = pd.read_sql_query("select * from country inner join asean on country.name=asean.negara", connection)
+
+labels = asean_df['Name']
+sizes = asean_df['SurfaceArea']
+
+pie = plt.subplot()
+pie.pie(sizes, labels=labels,autopct='%1.1f%%', startangle=0)
+pie.set_title('Presentase Luas Dataran ASEAN')
+pie.axis('equal')
+
+plt.show()
